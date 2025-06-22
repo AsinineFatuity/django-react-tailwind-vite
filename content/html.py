9f39f4aef71f4d97ee629bac7c9aa20ab2cc88d2
Kembo Franklyne <asininefatuity@gmail.com>
2025-06-22 13:54:40 +0300

    wrote the new html content

--- content/html.py
+++ content/html.py
@@ -1,29 +1,15 @@
-INDEX_HTML_CONTENT = """
-{% load static %}
-<!doctype html>
-<html lang="en" xml:lang="en">
-    <head>
-        <title>Home</title>
-    </head>
-    
-    <body>
-        <div id="root">
-        <script defer="defer" type="text/javascript" src="{% static 'index-bundle.js' %}"></script>
-    </body>
-</html>
-"""
-
 HOME_HTML_CONTENT = """
 {% load static %}
 <!doctype html>
 <html lang="en" xml:lang="en">
     <head>
+        {% vite_hmr_client %}
+        {% vite_asset 'frontend/index.tsx' %}
         <title>Home</title>
     </head>
     
     <body>
         <div id="root">
-        <script type="text/javascript" src="{% static 'index-bundle.js' %}"></script>
     </body>
 </html>
 """
