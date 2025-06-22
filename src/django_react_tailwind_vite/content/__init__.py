0693c3bfdadde306cddb2f2089e7b658038c6003
Kembo Franklyne <asininefatuity@gmail.com>
2025-06-22 17:12:47 +0300

    fixed all import issues

--- src/django_react_tailwind_vite/content/__init__.py
+++ src/django_react_tailwind_vite/content/__init__.py
@@ -1,6 +1,6 @@
-from content.package_json import PACKAGE_JSON_CONTENT
-from content.vite_config import VITE_CONFIG_CONTENT
-from content.requirements import REQUIREMENTS_CONTENT
+from .package_json import PACKAGE_JSON_CONTENT
+from .vite_config import VITE_CONFIG_CONTENT
+from .requirements import REQUIREMENTS_CONTENT
 from .dwango import (
     write_manage_py_content,
     write_django_asgi_content,
@@ -8,29 +8,29 @@ from .dwango import (
     write_django_settings_content,
     DJANGO_URLS_CONTENT
 )
-from content.html import HOME_HTML_CONTENT
-from content.static_dir import MAIN_CSS_CONTENT
-from content.gitinore import GITIGNORE_CONTENT
-from content.src_dir import INDEX_TSX_CONTENT, APP_TSX_CONTENT, HOME_PAGE_CONTENT
-from content.components import (
+from .html import HOME_HTML_CONTENT
+from .static_dir import MAIN_CSS_CONTENT
+from .gitinore import GITIGNORE_CONTENT
+from .src_dir import INDEX_TSX_CONTENT, APP_TSX_CONTENT, HOME_PAGE_CONTENT
+from .components import (
     COMPONENTS_INDEX_TSX_CONTENT,
     LOADING_INDICATOR_TSX_CONTENT,
     FEEDBACK_TOAST_TSX_CONTENT,
 )
-from content.environments import DEV_ENV_CONTENT, DEV_PROD_CONTENT
-from content.helpers_dir import INTERFACES_FILE_CONTENT, UTILS_FILE_CONTENT
-from content.redux_actions import (
+from .environments import DEV_ENV_CONTENT, DEV_PROD_CONTENT
+from .helpers_dir import INTERFACES_FILE_CONTENT, UTILS_FILE_CONTENT
+from .redux_actions import (
     ACTION_TYPES_CONTENT,
     FEEDBACK_TOAST_ACTION_CONTENT,
     LOADING_INDICATOR_ACTION_CONTENT,
     ACTIONS_INDEX_TS_CONTENT,
 )
-from content.redux_middleware import MIDDLEWARE_INDEX_CONTENT
-from content.redux_reducer import (
+from .redux_middleware import MIDDLEWARE_INDEX_CONTENT
+from .redux_reducer import (
     LOADING_INDICATOR_REDUCER_CONTENT,
     FEEDBACK_TOAST_REDUCER_CONTENT,
     REDUCER_INDEX_CONTENT,
 )
-from content.redux_store import REDUX_STORE_CONTENT
-from content.hooks import HOOKS_INDEX_TS_CONTENT
-from content.ts_config import TS_CONFIG_JSON_CONTENT
+from .redux_store import REDUX_STORE_CONTENT
+from .hooks import HOOKS_INDEX_TS_CONTENT
+from .ts_config import TS_CONFIG_JSON_CONTENT
