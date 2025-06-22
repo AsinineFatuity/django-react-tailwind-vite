2776c1c6edb359add8ef45527a310901d75f4d7b
Kembo Franklyne <asininefatuity@gmail.com>
2025-06-22 15:23:40 +0300

    better env naming

--- content/environments.py
+++ content/environments.py
@@ -1,9 +1,9 @@
 DEV_ENV_CONTENT = """
-REACT_APP_NODE_ENV=development
-REACT_APP_BASE_API_URL=http://127.0.0.1:8000
+VITE_NODE_ENV=development
+VITE_BASE_API_URL=http://127.0.0.1:8000
 """
 
 DEV_PROD_CONTENT = """ 
-REACT_APP_NODE_ENV=production
-REACT_APP_BASE_API_URL=http://your-production-url.com
+VITE_NODE_ENV=production
+VITE_BASE_API_URL=http://your-production-url.com
 """
