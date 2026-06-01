70671ce7346f2527589822199e1b5ae31c37f006
kembo <asininefatuity@gmail.com>
2026-06-01 14:12:35 +0300

    changes

--- src/django_react_tailwind_vite/content/vite_config.py
+++ src/django_react_tailwind_vite/content/vite_config.py
@@ -1,38 +1,42 @@
 VITE_CONFIG_CONTENT = """
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-import dotenv from 'dotenv'
+import { defineConfig} from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: "./frontend/.env" });
 
-// Load environment variables
-dotenv.config({ path: './frontend/environments/.env.development' })
 
 export default defineConfig({
-  base: '/static/',
-  plugins: [
-    tailwindcss()
-  ],
+  base: "/static/",
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './frontend'),
-      'images': path.resolve(__dirname, './frontend/src/images'),
+      "@": path.resolve(__dirname, "./frontend/src"),
+      images: path.resolve(__dirname, "./frontend/src/images"),
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    dedupe: ["react", "react-dom"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   build: {
-    outDir: path.resolve(__dirname, './assets'),
-    assetsDir: 'productionAssets',
+    outDir: path.resolve(__dirname, "./dist"),
+    assetsDir: "assets",
     emptyOutDir: true, // Empty the output directory before build
     manifest: "manifest.json",
     rollupOptions: {
       input: {
-        mainAssets: path.resolve(__dirname, './frontend/index.tsx'),
+        mainAssets: path.resolve(__dirname, "./frontend/index.tsx"),
       },
     },
-    sourcemap: true, // Generate source maps for development
+    sourcemap: false,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    hmr: true
   },
   define: {
-    'process.env': JSON.stringify(process.env),
+    "process.env": JSON.stringify(process.env),
   },
-})
+  envDir: "./frontend",
+});
 """
